ad13309bfd01a4eb4dfa4a4d298cbb92134d69ff
prathibha <prathibha.panga@gmail.com>
2017-09-23 22:52:35 -0700

    process env added for heroku deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
 var app = express();
-var port = 3000;
+app.set("port", process.env.PORT || 3000);
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -20,4 +20,6 @@ app.set("view engine", "handlebars");
 
 var routes = require('./controllers/burgers_controller.js');
 app.use('/',routes);
-app.listen(port);
+app.listen(app.get("port"), function(){
+    console.log("App is listening the port: " + app.get("port"));
+});
