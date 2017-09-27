ba62c9a105066a414279e3c399281f0110a4129d
prathibha <prathibha.panga@gmail.com>
2017-09-27 12:11:16 -0700

    connecting to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,29 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'burgers_db'
-});
+// var connection = mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'',
+//     database:'burgers_db'
+// });
+
+if (process.env.CLEARDB_DATABASE_URL) { // if you are using clearDB - you will use the cleardb url - see the config vars
+    // We are in production
+    connection= mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE_NAME
+    });
+} else {
+    // We are in a local environment
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "bb03b69f87c69c",
+        password: "2eb609c6",
+        database: "heroku_d3d4e2bebcc870d"
+    });
+}; 
 
 connection.connect(function(err){
     if(err){
