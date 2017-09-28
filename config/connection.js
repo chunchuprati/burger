b2376b62a6d91cfe9cb469a5db31aae6e56156f8
prathibha <prathibha.panga@gmail.com>
2017-09-27 18:23:40 -0700

    changed back

--- config/connection.js
+++ config/connection.js
@@ -5,15 +5,10 @@ var connection;
 if (process.env.CLEARDB_DATABASE_URL) { // if you are using clearDB - you will use the cleardb url - see the config vars
     // We are in production
     connection= mysql.createConnection({
-        host: 'us-cdbr-iron-east-05.cleardb.net',
-        user: 'bb03b69f87c69c',
-        password: '2eb609c6',
-        database: 'heroku_d3d4e2bebcc870d'
-
-        // host: process.env.HOST,
-        // user: process.env.USERNAME,
-        // password: process.env.PASSWORD,
-        // database: process.env.DATABASE_NAME
+        host: process.env.HOST,
+        user: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE_NAME
     });
 } else {
     // We are in a local environment
